8559505a5dec94685eb30d864087e0f4643c0438
l6_pc16 <l6_pc16@WIN-IM38HI1GTD6>
2021-11-11 09:31:22 +0300

    proje yüklendi

---
+++ adamAsmaca/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace adamAsmaca
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOyunBas = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // btnOyunBas
+            // 
+            this.btnOyunBas.Location = new System.Drawing.Point(614, 32);
+            this.btnOyunBas.Name = "btnOyunBas";
+            this.btnOyunBas.Size = new System.Drawing.Size(156, 45);
+            this.btnOyunBas.TabIndex = 0;
+            this.btnOyunBas.Text = "Oyuna Başla";
+            this.btnOyunBas.UseVisualStyleBackColor = true;
+            this.btnOyunBas.Click += new System.EventHandler(this.btnOyunBas_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(524, 100);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(12, 147);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(251, 61);
+            this.flowLayoutPanel2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.btnOyunBas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOyunBas;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+    }
+}
+
---
+++ adamAsmaca/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace adamAsmaca
+{
+    public partial class Form1 : Form
+    {
+        char[] alfabe = "ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZ".ToCharArray();
+        string[] sehirler = { "Ağrı", "Van", "Muş", "İzmir", "Burdur", "Ankara", "Sakarya" };
+        int rastgele;
+        string sehir;
+        Random r = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOyunBas_Click(object sender, EventArgs e)
+        {
+                       
+            rastgele = r.Next(0,7);
+            sehir=sehirler[rastgele];
+            for (int i = 0; i < alfabe.Length; i++)
+            {
+                Button btn = new Button();
+             //   btn.Name = alfabe[i].ToString();
+                btn.Text = alfabe[i].ToString();
+                btn.Width = 30;
+                btn.Height = 30;
+               // btn.Click += HarfButton_Click;
+               flowLayoutPanel1.Controls.Add(btn);
+            }
+
+            for (int i = 0; i <sehir.Length; i++)
+            {
+                TextBox txt = new TextBox();            
+                txt.Width = 25;
+                txt.Height = 25;
+                flowLayoutPanel2.Controls.Add(txt);
+            }
+
+            
+        }
+    }
+}
